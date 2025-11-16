c5d7bc1cd4da7108701606d389f4ae5eae0d0e82
Pallavi kumari <144532693+pallavi-vats@users.noreply.github.com>
2025-11-16 23:22:35 +0530

    Time: 0 ms (100%), Space: 12.6 MB (91.95%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        c=[]
+        for i in range(2):
+            c.extend(nums)
+        return c
+        
