4f4eeae32ab94a1ede2232e4abf418feda9dc9aa
Pallavi kumari <144532693+pallavi-vats@users.noreply.github.com>
2026-01-19 23:41:45 +0530

    Time: 0 ms (100%), Space: 13 MB (78.37%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d={}
+        for i in range(len(nums)):
+            curr=target-nums[i]
+            if curr in d:
+                return[d[curr],i]
+            d[nums[i]]=i    
